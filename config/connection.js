d98ee6a5901984f3a7b84cf65f36ce8d1569b240
Steve <stvchrm@steves-mbp.lan>
2024-07-13 14:19:17 -0600

    Add dialect to Sequelize configuration

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,11 @@ require('dotenv').config();
 const Sequelize = require('sequelize');
 
 const sequelize = process.env.NODE_ENV === 'production'
-  ? new Sequelize(process.env.DATABASE_URL)
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: 'postgres',
+      protocol: 'postgres',
+      logging: true
+    })
   : new Sequelize(
       process.env.DB_NAME,
       process.env.DB_USER,
@@ -10,7 +14,8 @@ const sequelize = process.env.NODE_ENV === 'production'
       {
         host: process.env.DB_HOST,
         dialect: 'postgres',
-        port: 5432
+        port: 5432,
+        logging: true
       }
     );
 
