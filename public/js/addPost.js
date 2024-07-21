43b0398f62ab258530ee5683c4b010d3810ffec4
Steve <stvchrm@steves-mbp.lan>
2024-07-21 13:03:03 -0600

    Update login logic

--- public/js/addPost.js
+++ public/js/addPost.js
@@ -1,25 +1,28 @@
-const newPostHandler = async (event) => {
-    event.preventDefault();
-  
-    const title = document.querySelector('input[name="post-title"]').value.trim();
-    const content = document.querySelector('textarea[name="post-content"]').value.trim();
-  
-    if (title && content) {
-      const response = await fetch('/api/posts', {
-        method: 'POST',
-        body: JSON.stringify({ title, content }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/dashboard');
-      } else {
-        alert('Failed to create post.');
+document.addEventListener('DOMContentLoaded', () => {
+  const addPostForm = document.querySelector('.add-post-form');
+
+  if (addPostForm) {
+    addPostForm.addEventListener('submit', async (event) => {
+      event.preventDefault();
+
+      const title = document.querySelector('#post-title').value.trim();
+      const content = document.querySelector('#post-content').value.trim();
+
+      if (title && content) {
+        const response = await fetch('/api/posts', {
+          method: 'POST',
+          body: JSON.stringify({ title, content }),
+          headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+          document.location.replace('/dashboard');
+        } else {
+          alert('Failed to add post.');
+        }
       }
-    }
-  };
-  
-  document
-    .querySelector('#new-post-form')
-    .addEventListener('submit', newPostHandler);
-  
+    });
+  } else {
+    console.error('Add post form not found');
+  }
+});
